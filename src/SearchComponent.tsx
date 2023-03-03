684497f9f4c37632ea7cf91a58afaeb58cbadcdf
tastetest <tastytothesupreme@gmail.com>
2023-03-03 10:41:40 -0700

    formatted

--- src/SearchComponent.tsx
+++ src/SearchComponent.tsx
@@ -2,12 +2,17 @@ import react from 'react';
 // import './assets/Main.css';
 import { Input, Space } from 'antd';
 
-const {Search} = Input;
-
 const SearchComponent = () => {
 	// do some string comparison to match routes
+	const { Search } = Input;
 	return (
-		<Search placeholder='Search something, like "cd about" ' />	)
-}
 
+		<Search
+			placeholder="input search text"
+			allowClear
+			enterButton="Search"
+			size="large"
+		/>
+	)
+}
 export default SearchComponent;
